c246fdd88f55b331468b3b7d218d69f5e06fa1e3
Shadirvan1 <shadirvant111@gamil.com>
2026-01-30 10:11:39 +0530

    add cors

--- e-commerce-backend/ecommerce_project_main/settings.py
+++ e-commerce-backend/ecommerce_project_main/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["e-tricktrove.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
    "https://trickstrove.onrender.com",
